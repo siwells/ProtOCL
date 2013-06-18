7da2a70a8589cee22641dcf62a483581889e868a
Simon Wells <siwells@gmail.com>
2013-06-18 11:16:37 +0100

    Added version 1 Java src

---
+++ v1/src/Driver.java
@@ -0,0 +1,31 @@
+
+
+public class Driver {
+
+    
+    public static void main(String arg[])
+    {
+	//DialogueGame dg=new DialogueGame();
+	//Player p1=new Player();
+	//Player p2=new Player();
+	//CommitmentStore s1=new CommitmentStore();
+        //CommitmentStore s2=new CommitmentStore();
+	//s1.addContent(new MoveContent());
+	//s2.addContent(new MoveContent());
+	//p1.setStore(s1);
+	//p1.setListenerStore(s2);
+       // p2.setStore(s2);
+	//p2.setListenerStore(s1);
+	//dg.setProponent(p1);
+	//dg.setOpponent(p2);
+	//dg.start();
+	
+	//test turn
+	Turn t=new Turn ();
+
+	t.addMove (new Move());
+        t.addMove (new Move());
+    }
+    
+   
+}
